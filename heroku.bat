9a6b5bd94f55836e6afb560e4d50c7430588e62b
jim-miller-gac <jim.miller.gac@gmail.com>
2017-01-12 10:34:31 -0600

    another update

--- heroku.bat
+++ heroku.bat
@@ -3,5 +3,5 @@ git commit -am "another update"
 git push heroku master
 
 REM Copy to the Raspberry Pi share.
-REM copy server.js Z:\
-REM copy package.json Z:\
+copy server.js Z:\
+copy package.json Z:\
